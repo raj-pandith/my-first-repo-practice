66e3dbbc00d1f37a02a6de4caa150201e5fb2fd9
keerthana1234p-glitch <keerthana1234p@gmail.com>
2026-08-03 15:48:35 +0530

    Add my first python file hello.py
    Removed duplicate print statement and added a comment.

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 print("Hello from github!")
 print("This is your first python code")
 name=input("what's your name?")
-print("Nice to meet you, "+name)
+print("Nice to meet you, "+name) 
+#fixed typo
